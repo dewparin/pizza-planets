b2a0d16ced4a75100e3446906d1e6742e4570a75
DewP <parinsura@gmail.com>
2026-03-05 10:31:54 +0700

    Implement PlanetRepository and configure Room in Koin
    - Created `PlanetRepository` interface and `OfflinePlanetRepository` implementation
    - Added error handling for planet retrieval using Kotlin Flow
    - Configured Koin to provide `PizzaPlanetsDatabase`, `PlanetDao`, and `PlanetRepository` instances

---
+++ app/src/main/java/com/example/pizzaplanets/data/PlanetRepository.kt
@@ -0,0 +1,33 @@
+package com.example.pizzaplanets.data
+
+import android.util.Log
+import com.example.pizzaplanets.data.local.PlanetDao
+import com.example.pizzaplanets.entity.Planet
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import java.io.IOException
+
+interface PlanetRepository {
+    fun getAllPlanets(): Flow<List<Planet>>
+}
+
+class OfflinePlanetRepository(
+    private val planetDao: PlanetDao,
+) : PlanetRepository {
+
+    companion object {
+        const val TAG = "OfflinePlanetRepository"
+    }
+
+    override fun getAllPlanets(): Flow<List<Planet>> = planetDao
+        .queryAllPlanets()
+        .catch {
+            if (it is IOException) {
+                Log.e(TAG, "getAllPlanets # Error reading planet list")
+                emit(listOf())
+            } else {
+                throw it
+            }
+        }
+
+}
--- app/src/main/java/com/example/pizzaplanets/di/AppModule.kt
+++ app/src/main/java/com/example/pizzaplanets/di/AppModule.kt
@@ -1,7 +1,29 @@
 package com.example.pizzaplanets.di
 
+import androidx.room.Room
+import com.example.pizzaplanets.data.OfflinePlanetRepository
+import com.example.pizzaplanets.data.PlanetRepository
+import com.example.pizzaplanets.data.local.PizzaPlanetsDatabase
+import com.example.pizzaplanets.data.local.PlanetDao
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
-    // TODO: Declare dependencies
+
+    // Room Database and DAO
+    single<PizzaPlanetsDatabase> {
+        Room.databaseBuilder(
+            androidContext(),
+            PizzaPlanetsDatabase::class.java,
+            "pizza_planets_database",
+        ).build()
+    }
+    single<PlanetDao> {
+        get<PizzaPlanetsDatabase>().planetDao()
+    }
+
+    // Repository
+    single<PlanetRepository> {
+        OfflinePlanetRepository(get())
+    }
 }

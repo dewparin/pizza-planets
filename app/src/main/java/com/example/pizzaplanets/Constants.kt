abfaa7e00f598ad98775e169e2a175ca0549417c
DewP <parinsura@gmail.com>
2026-03-09 19:53:12 +0700

    Implement CookingOrderWorker and chain it in OrderRepository
    - Implemented `CookingOrderWorker` to simulate the cooking process and update order status to `COOKING`
    - Updated `OrderRepository` to chain `CookingOrderWorker` after `ConfirmOrderWorker` using WorkManager
    - Added `COOKING_ORDER_DELAY_MILLIS` and `DELIVERY_ORDER_DELAY_MILLIS` constants for simulated delays

--- app/src/main/java/com/example/pizzaplanets/Constants.kt
+++ app/src/main/java/com/example/pizzaplanets/Constants.kt
@@ -5,3 +5,5 @@ package com.example.pizzaplanets
 const val KEY_ORDER_ID = "KEY_ORDER_ID"
 const val PRE_WORK_DELAY_MILLIS: Long = 3000
 const val CONFIRM_ORDER_DELAY_MILLIS: Long = 2000
+const val COOKING_ORDER_DELAY_MILLIS: Long = 5_000
+const val DELIVERY_ORDER_DELAY_MILLIS: Long = 7_000
--- app/src/main/java/com/example/pizzaplanets/data/OrderRepository.kt
+++ app/src/main/java/com/example/pizzaplanets/data/OrderRepository.kt
@@ -7,6 +7,7 @@ import androidx.work.workDataOf
 import com.example.pizzaplanets.KEY_ORDER_ID
 import com.example.pizzaplanets.data.local.OrderDao
 import com.example.pizzaplanets.data.worker.ConfirmOrderWorker
+import com.example.pizzaplanets.data.worker.CookingOrderWorker
 import com.example.pizzaplanets.entity.result.OrderQueryResult
 import kotlinx.coroutines.flow.Flow
 
@@ -29,6 +30,10 @@ class OfflineOrderRepository(
                 uniqueWorkName = "ORDER#$orderId",
                 existingWorkPolicy = ExistingWorkPolicy.REPLACE,
                 request = OneTimeWorkRequestBuilder<ConfirmOrderWorker>()
+                    .setInputData(workDataOf(KEY_ORDER_ID to orderId))
+                    .build()
+            ).then(
+                OneTimeWorkRequestBuilder<CookingOrderWorker>()
                     .setInputData(workDataOf(KEY_ORDER_ID to orderId))
                     .build()
             ).apply {

390f234df2f0e61a179701264cdd21c2d9366f92
DewP <parinsura@gmail.com>
2026-03-16 15:53:53 +0700

    Add data module dependency to food feature
    - Added `:data` project dependency to the `food` feature module

--- feature/food/build.gradle.kts
+++ feature/food/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 
 dependencies {
     implementation(project(":core:ui"))
+    implementation(project(":data"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
